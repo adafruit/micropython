41acb4ee61c8b887c5df901e52f5902a3fad184a
Dan Halbert <halbert@halwitz.org>
2017-06-07 05:44:06 -0400

    #define MICROPY_PY_MICROPYTHON_MAX_STACK_USE in mpconfig.h, not just in atmel-samd/mpconfigport.h

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -774,6 +774,11 @@ typedef double mp_float_t;
 #define MICROPY_PY_MICROPYTHON_MEM_INFO (0)
 #endif
 
+// Whether to provide maximum stack excursion checking functions in micropython module
+#ifndef MICROPY_PY_MICROPYTHON_MAX_STACK_USE
+#define MICROPY_PY_MICROPYTHON_MAX_STACK_USE (0)
+#endif
+
 // Whether to provide "array" module. Note that large chunk of the
 // underlying code is shared with "bytearray" builtin type, so to
 // get real savings, it should be disabled too.
