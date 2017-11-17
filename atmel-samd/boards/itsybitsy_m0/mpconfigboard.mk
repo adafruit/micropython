b88c2b3d7a2674bd23ac5136973bddf01c7ffb82
ladyada <limor@ladyada.net>
2017-11-17 00:15:36 -0500

    add SPI flash support

--- atmel-samd/boards/itsybitsy_m0/mpconfigboard.mk
+++ atmel-samd/boards/itsybitsy_m0/mpconfigboard.mk
@@ -1,7 +1,7 @@
-LD_FILE = boards/samd21x18-bootloader-crystalless.ld
+LD_FILE = boards/samd21x18-bootloader-external-flash-crystalless.ld
 USB_VID = 0x239A
-USB_PID = 0x801F
+USB_PID = 0x8012
 
-INTERNAL_FLASH_FILESYSTEM = 1
+SPI_FLASH_FILESYSTEM = 1
 
 CHIP_VARIANT = SAMD21G18A
