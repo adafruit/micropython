2e36eff5a9fa609e53b70aa92151dd7ebf2da9cb
sabas1080 <s@theinventorhouse.org>
2019-03-10 21:13:09 -0600

    SPI default

--- ports/atmel-samd/boards/arduino_mkr1010/mpconfigboard.h
+++ ports/atmel-samd/boards/arduino_mkr1010/mpconfigboard.h
@@ -14,9 +14,9 @@
 #define DEFAULT_I2C_BUS_SCL (&pin_PA09)
 #define DEFAULT_I2C_BUS_SDA (&pin_PA08)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PA13)
+#define DEFAULT_SPI_BUS_SCK (&pin_PA15)
 #define DEFAULT_SPI_BUS_MOSI (&pin_PA12)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA15)
+#define DEFAULT_SPI_BUS_MISO (&pin_PA13)
 
 #define DEFAULT_UART_BUS_RX (&pin_PB23)
 #define DEFAULT_UART_BUS_TX (&pin_PB22)
