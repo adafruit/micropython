21c92a58232e5ab97793269704749845fcc6db54
Seth Kerr <skerr@aggies.ncat.edu>
2020-11-19 20:22:41 -0500

    CP Sapling board stuffs for final release

--- ports/atmel-samd/boards/CP_Sapling_m0/mpconfigboard.h
+++ ports/atmel-samd/boards/CP_Sapling_m0/mpconfigboard.h
@@ -15,6 +15,7 @@
 #define IGNORE_PIN_PA07	    1
 #define IGNORE_PIN_PA12     1
 #define IGNORE_PIN_PA13     1
+#define IGNORE_PIN_PA14	    1
 #define IGNORE_PIN_PA20     1
 #define IGNORE_PIN_PA21     1
 // USB is always used internally so skip the pin objects for it.
