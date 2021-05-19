2cd9b5573cd2cbddf514ad5465aa5479682c3345
JoeBakalor <jmbakalor@gmail.com>
2021-05-19 11:44:36 -0400

    added servo pin definitions to Bluebird, added SD 7.0.1 for bluebird

--- ports/nrf/boards/teknikio_bluebird/pins.c
+++ ports/nrf/boards/teknikio_bluebird/pins.c
@@ -57,6 +57,9 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_ACCELEROMETER_INTERRUPT), MP_ROM_PTR(&pin_P1_10) },
     { MP_ROM_QSTR(MP_QSTR_ACCELEROMETER_SDA),       MP_ROM_PTR(&pin_P1_12) },
     { MP_ROM_QSTR(MP_QSTR_ACCELEROMETER_SCL),       MP_ROM_PTR(&pin_P1_11) },
+
+    { MP_ROM_QSTR(MP_QSTR_SERVO_LEFT),              MP_ROM_PTR(&pin_P0_13) },
+    { MP_ROM_QSTR(MP_QSTR_SERVO_RIGHT),             MP_ROM_PTR(&pin_P0_15) },
 };
 
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
